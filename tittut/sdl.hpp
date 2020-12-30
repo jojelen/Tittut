27e3b5852371c433b6b074609ce8e7f3bc0152d8
Joel Oredsson <joel_ored@hotmail.com>
2020-12-30 13:39:45 +0100

    Create SDL error function

--- tittut/sdl.hpp
+++ tittut/sdl.hpp
@@ -7,11 +7,17 @@
 #include <iostream>
 #include <string>
 
+void sdlError(std::string msg) {
+    std::string sdlErrorMsg(SDL_GetError());
+    SDL_ClearError();
+    throw std::runtime_error(msg + ": " + sdlErrorMsg);
+}
+
 void initSDL() {
     static bool initialized = false;
     if (!initialized) {
         if (SDL_Init(SDL_INIT_VIDEO)) {
-            throw std::runtime_error(std::string("Could not initialize SDL: ") + SDL_GetError());
+            sdlError("SDL_Init");
         }
         std::atexit(SDL_Quit);
         initialized = true;
@@ -24,67 +30,61 @@ void initSDL() {
 class SDLWindow {
 private:
     std::string name_;
-  SDL_Window *win_ = nullptr;
-  SDL_Renderer *ren_ = nullptr;
-  SDL_Texture *texture_ = nullptr;
-  SDL_Rect rect_ = {};
-  bool quit_;
-  V4L v4l_;
+    SDL_Window *win_ = nullptr;
+    SDL_Renderer *ren_ = nullptr;
+    SDL_Texture *texture_ = nullptr;
+    SDL_Rect rect_ = {};
+    bool quit_;
+    V4L v4l_;
 
 public:
-  SDLWindow(const std::string &name, int width, int height) :
-      name_(name), quit_(false), v4l_(width, height, V4L2_PIX_FMT_YUYV) {
-    initSDL();
+    SDLWindow(const std::string &name, int width, int height) :
+              name_(name), quit_(false),
+              v4l_(width, height, V4L2_PIX_FMT_YUYV) {
+        initSDL();
 
-    win_ = SDL_CreateWindow(name_.c_str(), 100, 100, width, height,
-                            SDL_WINDOW_SHOWN);
-    if (win_ == nullptr) {
-      std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-      SDL_Quit();
-      return;
-    }
+        win_ = SDL_CreateWindow(name_.c_str(), 100, 100, width, height,
+                                SDL_WINDOW_SHOWN);
+        if (win_ == nullptr) {
+            sdlError("SDL_CreateWindow");
+        }
 
-    ren_ = SDL_CreateRenderer(
-        win_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-    if (ren_ == nullptr) {
-      SDL_DestroyWindow(win_);
-      std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
-      SDL_Quit();
-      return;
-    }
+        ren_ = SDL_CreateRenderer(
+            win_, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+        if (ren_ == nullptr) {
+            SDL_DestroyWindow(win_);
+            sdlError("SDL_CreateRenderer");
+        }
 
-    texture_ = SDL_CreateTexture(ren_, SDL_PIXELFORMAT_YUY2,
-                                 SDL_TEXTUREACCESS_STREAMING, width, height);
-    if (texture_ == nullptr) {
-      SDL_DestroyWindow(win_);
-      std::cout << "SDL_CreateTexture Error: " << SDL_GetError() << std::endl;
-      SDL_Quit();
-      return;
-    }
+        texture_ = SDL_CreateTexture(ren_, SDL_PIXELFORMAT_YUY2,
+                                     SDL_TEXTUREACCESS_STREAMING,
+                                     width,
+                                     height);
+        if (texture_ == nullptr) {
+            SDL_DestroyWindow(win_);
+            sdlError("SDL_CreateTexture");
+        }
 
-    rect_.w = width;
-    rect_.h = height;
+        rect_.w = width;
+        rect_.h = height;
   }
 
-  ~SDLWindow() {
-    SDL_DestroyWindow(win_);
-    const char *error = SDL_GetError();
-    if (*error) {
-      SDL_Log("SDL error: %s", error);
-      SDL_ClearError();
+    ~SDLWindow() {
+        SDL_DestroyWindow(win_);
     }
-  }
 
   void updateTexture(void *buffer) {
     if (SDL_UpdateTexture(texture_, &rect_, buffer, rect_.w * 2))
     {
-        throw std::runtime_error("Failed updating texture");
+        sdlError("SDL_UpdateTexture");
     }
   }
 
   void render() {
-    SDL_RenderClear(ren_);
-    SDL_RenderCopy(ren_, texture_, NULL, &rect_);
+    if (SDL_RenderClear(ren_))
+        sdlError("SDL_RenderClear");
+    if (SDL_RenderCopy(ren_, texture_, NULL, &rect_))
+        sdlError("SDL_RenderCopy");
     SDL_RenderPresent(ren_);
   }
 
@@ -95,12 +95,10 @@ public:
         switch (event.type) {
         case SDL_KEYDOWN:
           if (event.key.keysym.sym == SDLK_ESCAPE) {
-            std::cout << "Pressed quit...\n";
             quit_ = true;
           }
           break;
         case SDL_QUIT:
-          std::cout << "Quitting...\n";
           quit_ = true;
           break;
         }
